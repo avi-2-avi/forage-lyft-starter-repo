3bf910c8d1511624322414ffd50c9529bfd4fb66
avi-2-avi <avicon95@protonmail.com>
2023-01-26 21:26:56 -0500

    Corrected battery references

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,13 +1,13 @@
 from abc import ABC
-from datetime import datetime
-from ..battery import Battery
+from datetime import datetime, date
+from battery import Battery
 
 class NubbinBattery(Battery, ABC):
-    def __init__(self, last_service_date, current_date):
+    def __init__(self, last_service_date: date, current_date: date):
         self.last_service_date = last_service_date
         self.current_date = current_date
 
-    def needs_service(self):
+    def needs_service(self) -> bool:
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
         if service_threshold_date < datetime.today().date() or self.needs_service():
             return True
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,9 +1,9 @@
 from abc import ABC
-from datetime import datetime
-from ..battery import Battery
+from datetime import datetime, date
+from battery import Battery
 
 class SpindlerBattery(Battery, ABC):
-    def __init__(self, last_service_date, current_date):
+    def __init__(self, last_service_date: date, current_date: date):
         self.last_service_date = last_service_date
         self.current_date = current_date
 
